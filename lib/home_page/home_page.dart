1a9299ab406aa13247fee93bf54040a900e8ece5
NassimBoufalous <boufalousnassim1@gmail.com>
2023-10-13 14:56:48 +0200

    Nassim

--- lib/home_page/home_page.dart
+++ lib/home_page/home_page.dart
@@ -2,8 +2,6 @@ import 'package:app/weapon_page/weapon_page.dart';
 import 'package:app/widget.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(const BottomNavigationBarExampleApp());
-
 class BottomNavigationBarExampleApp extends StatelessWidget {
   const BottomNavigationBarExampleApp({Key? key}) : super(key: key);
 
@@ -11,18 +9,6 @@ class BottomNavigationBarExampleApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: BottomNavigationBarExample(),
-    );
-  }
-
-  void main() {
-    runApp(
-      MaterialApp(
-        initialRoute: '/',
-        routes: {
-          '/': (context) => const BottomNavigationBarExampleApp(),
-          '/secondPage': (context) => WeaponPage(),
-        },
-      ),
     );
   }
 }
